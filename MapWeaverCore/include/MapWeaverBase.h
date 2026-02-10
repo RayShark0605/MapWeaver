db1d02de6f6e8fd9394cbbd89543e35bc0eec82e
xiarui <xiarui@zwcad.com>
2026-02-10 11:28:15 +0800

    提交MapWeaverBase

---
+++ MapWeaverCore/include/MapWeaverBase.h
@@ -0,0 +1,26 @@
+﻿#ifndef MAP_WEAVER_BASE_H
+#define MAP_WEAVER_BASE_H
+
+#include "MapWeaverPort.h"
+#include <string>
+
+MAPWEAVERCORE_PORT void InitProjSearchPath(const std::string& projDataDirUtf8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ MapWeaverCore/src/MapWeaverBase.cpp
@@ -0,0 +1,12 @@
+﻿#include "MapWeaverBase.h"
+#include "ogr_srs_api.h"
+
+void InitProjSearchPath(const std::string& projDataDirUtf8)
+{
+    const char* projSearchPaths[] =
+    {
+        projDataDirUtf8.c_str(),
+        nullptr
+    };
+    OSRSetPROJSearchPaths(projSearchPaths);
+}
