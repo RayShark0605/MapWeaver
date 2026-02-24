c7b6279abce54f2e46206e8f9bb3d503745b102a
xiarui <xiarui@zwcad.com>
2026-02-24 16:39:21 +0800

    修复Test中的main.cpp

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,10 +1,10 @@
 ﻿#include "../MapWeaverCore/include/GeoCrsManager.h"
 #include "../MapWeaverCore/include/GeoBoundingBox.h"
 #include "../MapWeaverCore/include/GeoCrsTransform.h"
-#include "../GlobalBase/GB_Logger.h"
-#include "../GlobalBase/GB_SmbAccessor.h"
-#include "../GlobalBase/GB_Interval.h"
-#include "../GlobalBase/GB_DateTime.h"
+#include "GB_Logger.h"
+#include "GB_SmbAccessor.h"
+#include "GB_Interval.h"
+#include "GB_DateTime.h"
 #include <iostream>
 
 int main(int argc, char* argv[])
