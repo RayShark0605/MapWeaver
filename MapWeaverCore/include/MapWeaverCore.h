8a4e9193967ffc553f340a8402994f4a582753a9
xiarui <xiarui@zwcad.com>
2026-02-24 16:54:14 +0800

    新增MapWeaverCore

---
+++ MapWeaverCore/include/MapWeaverCore.h
@@ -0,0 +1,24 @@
+﻿#ifndef MAP_WEAVER_CORE_H
+#define MAP_WEAVER_CORE_H
+
+#include "MapWeaverPort.h"
+#include "GB_Network.h"
+#include <string>
+
+MAPWEAVERCORE_PORT bool DownloadWmsCapabilities(const std::string& urlUtf8, std::string& outCapabilitiesXmlUtf8, const GB_NetworkRequestOptions& options = GB_NetworkRequestOptions());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ MapWeaverCore/src/MapWeaverCore.cpp
@@ -0,0 +1,8 @@
+﻿#include "MapWeaverCore.h"
+
+bool DownloadWmsCapabilities(const std::string& urlUtf8, std::string& outCapabilitiesXmlUtf8, const GB_NetworkRequestOptions& options)
+{
+
+}
+
+
