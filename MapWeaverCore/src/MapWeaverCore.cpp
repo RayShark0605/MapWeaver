eb0a8f46c386a3f234d831b01a3466b619e7236b
RayShark0605 <gallyonstoavug@gmail.com>
2026-02-25 22:44:19 +0800

    添加.pem证书，完成XML的下载和保存。

--- MapWeaverCore/src/MapWeaverCore.cpp
+++ MapWeaverCore/src/MapWeaverCore.cpp
@@ -5,8 +5,31 @@
 
 bool IsUrlForWMTS(const std::string& urlUtf8)
 {
-	return GB_Utf8Find(urlUtf8, GB_STR("SERVICE=WMTS"), false) > 0
-		&& GB_Utf8Find(urlUtf8, GB_STR("/WMTSCapabilities.xml"), false);
+	// 包含 SERVICE=WMTS 键值对（KVP风格）
+	if (GB_Utf8Find(urlUtf8, GB_STR("SERVICE=WMTS"), false) > 0)
+	{
+		return true;
+	}
+
+	// RESTful 风格的 WMTS
+	if (GB_Utf8Find(urlUtf8, GB_STR("/WMTSCapabilities.xml"), false) > 0)
+	{
+		return true;
+	}
+
+	// 匹配 "/wmts?", "/wmts/" 
+	if (GB_Utf8Find(urlUtf8, GB_STR("/wmts?"), false) > 0 || GB_Utf8Find(urlUtf8, GB_STR("/wmts/"), false) > 0)
+	{
+		return true;
+	}
+
+	// 匹配正好以 "/wmts" 结尾的情况
+	if (GB_Utf8EndsWith(urlUtf8, GB_STR("/wmts"), false))
+	{
+		return true;
+	}
+
+	return false;
 }
 
 bool DownloadWmsCapabilities(const std::string& rawUrlUtf8, std::string& outCapabilitiesXmlUtf8, const GB_NetworkRequestOptions& options)
@@ -18,14 +41,25 @@ bool DownloadWmsCapabilities(const std::string& rawUrlUtf8, std::string& outCapa
 		urlUtf8 = GB_UrlOperator::SetUrlQueryValue(urlUtf8, GB_STR("REQUEST"), GB_STR("GetCapabilities"));
 	}
 
-	const GB_NetworkResponse response = GB_RequestUrlData(urlUtf8, options);
-	if (!response.ok)
+	GB_NetworkResponse response = GB_RequestUrlData(urlUtf8, options);
+	if (response.ok)
 	{
-		return false;
+		outCapabilitiesXmlUtf8 = response.body;
+		return true;
 	}
 
-	outCapabilitiesXmlUtf8 = response.body;
-	return true;
+	GBLOG_WARNING(GB_Utf8Format("Failed to download WMS capabilities from URL: '%s'. HTTP status code: %ld. Error message: %s", urlUtf8.c_str(), response.httpStatusCode, response.errorMessageUtf8.c_str()));
+
+	response = GB_RequestUrlData(rawUrlUtf8, options);
+	if (response.ok)
+	{
+		outCapabilitiesXmlUtf8 = response.body;
+		return true;
+	}
+
+	GBLOG_WARNING(GB_Utf8Format("Failed to download WMS capabilities from original URL: '%s'. HTTP status code: %ld. Error message: %s", rawUrlUtf8.c_str(), response.httpStatusCode, response.errorMessageUtf8.c_str()));
+
+	return false;
 }
 
 
--- Test/main.cpp
+++ Test/main.cpp
@@ -1,21 +1,27 @@
-﻿#include "../MapWeaverCore/include/GeoCrsManager.h"
-#include "../MapWeaverCore/include/GeoBoundingBox.h"
-#include "../MapWeaverCore/include/GeoCrsTransform.h"
-#include "../MapWeaverCore/include/MapWeaverCore.h"
+﻿#include "../MapWeaverCore/include/MapWeaverCore.h"
 #include "GB_Logger.h"
-#include "GB_SmbAccessor.h"
+#include "GB_FileSystem.h"
 #include "GB_Interval.h"
 #include "GB_DateTime.h"
+#include "GB_IO.h"
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
 	GB_SetConsoleEncodingToUtf8();
+	GB_SetLogToConsole(true);
 
 	std::string capabilitiesXmlUtf8 = "";
-	bool ok = DownloadWmsCapabilities(GB_STR("https://gsi-cyberjapan.github.io/experimental_wmts/gsitiles_wmts_light.xml"), capabilitiesXmlUtf8);
+	if (!DownloadWmsCapabilities(GB_STR("https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/1.0.0/WMTSCapabilities.xml"), capabilitiesXmlUtf8)
+		|| capabilitiesXmlUtf8.empty())
+	{
+		return 1;
+	}
 
-
+	if (!GB_WriteUtf8ToFile(GB_GetExeDirectory() + GB_STR("capabilities.xml"), capabilitiesXmlUtf8, false))
+	{
+		return 1;
+	}
 	return 0;
 }
 
